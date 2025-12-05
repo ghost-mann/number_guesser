6195b0c7ce8b12179d246e6addc242c6e8ef54c3
ghost-mann <iamaob8@protonmail.com>
2025-12-05 12:32:33 +0300

    feat: added variables to store secret number, the guessed number, number of guesses in main.c

--- main.c
+++ main.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    // random number every time
+    srand(time(0));
+
+    // secret number between 1-100
+    int secret = rand() % 100 + 1;
+    // where the guess is stored
+    int guess;
+    // times a player has guessed - starts at zero
+    int tries = 0;
+
+    printf("I have picked a random number between 1 and 100.\n");
+    printf("can you guess it?");
+
+    while(1) {
+
+    }
+
+}
+
